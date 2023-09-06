fd968ac1f34bec33f50ec5d4d8ca1e7044f0df83
Luis Gonzalez <47365211+Luis7222@users.noreply.github.com>
2023-09-06 20:18:29 +0000

    this is now a java test project

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -1,12 +1,29 @@
 package project1;
 
+// public class Main {
+
+//     public static void main(String[] args) {
+
+//     project entry1 = new project("Luis7222","100221");
+
+
+
+//      }
+// }
+
+
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Create a stream of strings
+        Stream<String> words = Stream.of("Hello", "world!","test");
 
-    project entry1 = new project("Luis7222","100221");
+        // Count the number of words in the stream
+        long wordCount = words.count();
 
-
-
-     }
+        // Print the number of words
+        System.out.println(wordCount);
+    }
 }
